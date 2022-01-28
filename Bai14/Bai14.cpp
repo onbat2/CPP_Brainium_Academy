eb95948c8c3fa0ba946112e5e1366a2ed3bf18a1
onbat2 <97330629+onbat2@users.noreply.github.com>
2022-01-28 14:30:36 +0700

    Add files via upload

---
+++ Bai14/Bai14.cpp
@@ -0,0 +1,33 @@
+//Bai14: Toán tử so sánh 3 chiều
+
+#include <iostream>
+#include <compare>
+
+using namespace std;
+
+int main() {
+    int x = 100;
+    int y = 500;
+    // sử dụng toán tử so sánh ba chiều
+    auto result = x <=> y;
+    if (result < 0) {
+        cout << "x < y" << endl;
+    }
+    else if (result == 0) {
+        cout << "x = y" << endl;
+    }
+    else {
+        cout << "x > y" << endl;
+    }
+    // hoặc tương đương:
+    if (result == strong_ordering::less) {
+        cout << "x < y" << endl;
+    }
+    else if (result == strong_ordering::equal) {
+        cout << "x = y" << endl;
+    }
+    else if (result == strong_ordering::greater) {
+        cout << "x > y" << endl;
+    }
+}
+// Kết quả: x < y

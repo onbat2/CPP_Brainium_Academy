5e05d899ada68fc4c87980e5d34e8f91c4673da7
onbat2 <quangduc11202@gmail.com>
2022-03-25 16:38:07 +0700

    update

---
+++ Exercises/E09/E09-1.cpp
@@ -0,0 +1,19 @@
+/*
+Bài1: Viết chương trình cho phép nhập vào một số nguyên n.
+      Hãy cho biết số đó là số chẵn hay số lẻ?
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Nhập số nguyên n: ";
+    cin >> n;
+    if (n % 2 == 0){
+        cout << n << " là số chẵn" << endl;
+    }else{
+        cout << n << " là số lẻ" << endl;
+    }
+}
